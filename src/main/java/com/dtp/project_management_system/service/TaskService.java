77ed3020337b73f6e833c913a28c0502590e6c03
Dao Tan Phat <daotanphat2143@gmail.com>
2025-05-11 17:06:47 +0700

    feat: Chat AI project's status

--- src/main/java/com/dtp/project_management_system/service/TaskService.java
+++
@@ -1,7 +0,0 @@
-package com.dtp.project_management_system.service;
-
-import com.dtp.project_management_system.dto.TaskData;
-
-public interface TaskService {
-    TaskData getTaskData(Long projectId);
-}
